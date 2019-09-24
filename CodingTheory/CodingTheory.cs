9699f435692bff12438ab4e503a58a23bd94545f
Ильназ Хайруллин <ilnaz_khairullin@mail.ru>
2019-09-24 22:26:45 +0300

    CodingTheory

---
+++ CodingTheory/CodingTheory.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodingTheory
+{
+    /// <summary>
+    /// Наука о кодирования.
+    /// </summary>
+    public static class CodingTheory
+    {
+        /// <summary>
+        /// Перевод из десятичной системы.
+        /// </summary>
+        /// <param name="number"></param>
+        /// <param name="system"></param>
+        /// <returns></returns>
+        public static List<int> TranslationFromDecimal(int number, int system)
+        {
+            var numbers = new List<int>();
+
+            for (var current = number; current > 0; current /= system)
+            {
+                numbers.Add(current % system);
+            }
+
+            numbers.Reverse();
+
+            return numbers;
+        }
+        /// <summary>
+        /// Перевод в десятичную систему.
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <param name="system"></param>
+        /// <returns></returns>
+        public static int TranslationToDecimal(List<int> numbers, int system)
+        {
+            var result = 0;
+
+            for (var index = 0; index < numbers.Count; index++)
+            {
+                var current = numbers[(numbers.Count - 1) - index];
+
+                result += (int)(current * Math.Pow(system, index));
+            }
+
+            return result;
+        }
+    }
+}
---
+++ CodingTheory/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodingTheory
+{
+    internal class Program
+    {
+        private static void ShowTranslationFromDecimal(int number, int system)
+        {
+            foreach (var current in CodingTheory.TranslationFromDecimal(number, system))
+            {
+                Console.Write($"{current}");
+            }
+
+            Console.WriteLine();
+        }
+        private static void ShowTranslationToDecimal(List<int> numbers, int system)
+        {
+            Console.WriteLine(CodingTheory.TranslationToDecimal(numbers, system));
+        }
+        private static void Main(string[] args)
+        {
+            // Перевод из десятичной системы счисления, в двоичную систему счисления.
+            ShowTranslationFromDecimal(19, 2);
+
+            Console.WriteLine();
+
+            // Перевод в десятичную систему счисления, из двоичной системы счисления.
+            ShowTranslationToDecimal(new List<int>() { 1, 0, 0, 1, 1 }, 2);
+
+            Console.ReadKey();
+        }
+    }
+}
